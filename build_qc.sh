2e1fb50a5a7d824beadf0ee7423f80b11fc1c7f8
JianLIUhep <jian.liu@cern.ch>
2021-01-15 18:27:49 +0100

    add QCanalysis

--- build_qc.sh
+++ build_qc.sh
@@ -13,6 +13,9 @@ git clone https://github.com/AliceO2Group/DataDistribution.git
 git clone https://github.com/alisw/alidist.git
 #alibuild
 git clone https://github.com/alisw/alibuild.git
+#QCanalysis
+https://github.com/iravasen/QCanalysis.git
+
 
 #Building 
 alibuild/aliBuild build O2 QualityControl Readout DataDistribution  --defaults o2 --jobs 8 #One may need to reduce the jobs to < 4 if the RAM is less than 8 GB
